0b73f78da940b0b4526a08cb24ece667f0a50678
MunavarPM <121251398+MunavarPM@users.noreply.github.com>
2023-08-15 17:40:49 +0530

    Update and rename LeetCode.2806 to LeetCode_2806.swift

--- LeetCode_2806.swift
+++ LeetCode_2806.swift
@@ -1,3 +1,4 @@
+/*
 QUESTION.2806
 Initially, you have a bank account balance of 100 dollars.
 
@@ -9,10 +10,9 @@ If there is more than one nearest multiple of 10, the largest multiple is chosen
 
 Return an integer denoting your account balance after making a purchase worth purchaseAmount dollars from the store.
 
-Note: 0 is considered to be a multiple of 10 in this problem.
+Note: 0 is considered to be a multiple of 10 in this problem. 
+*/
 
-
-CODE:
 class Solution {
     func accountBalanceAfterPurchase(_ purchaseAmount: Int) -> Int {
         switch (purchaseAmount % 10) {
