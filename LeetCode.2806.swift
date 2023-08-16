007247c9a3d859cba888eb17786bd46fbe2fb956
MunavarPM <121251398+MunavarPM@users.noreply.github.com>
2023-08-16 20:58:04 +0530

    Create LeetCode.2806.swift

---
+++ LeetCode.2806.swift
@@ -0,0 +1,12 @@
+/*
+You are given a positive integer arrivalTime denoting the arrival time of a train in hours, and another positive integer delayedTime denoting the amount of delay in hours.
+
+Return the time when the train will arrive at the station.
+
+Note that the time in this problem is in 24-hours format. */
+
+class Solution {
+    func findDelayedArrivalTime(_ arrivalTime: Int, _ delayedTime: Int) -> Int {
+        (arrivalTime + delayedTime) % 24
+    }
+}
