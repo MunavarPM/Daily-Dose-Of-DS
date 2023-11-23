f03bf9ee80aba907d80651bab4baba0ca2e60d8c
MunavarPM <121251398+MunavarPM@users.noreply.github.com>
2023-11-23 21:32:44 +0530

    Update LeetCode_2806.swift

--- LeetCode_2806.swift
+++ LeetCode_2806.swift
@@ -15,6 +15,7 @@ Note: 0 is considered to be a multiple of 10 in this problem.
 
 */
 
+
 class Solution {
     func accountBalanceAfterPurchase(_ purchaseAmount: Int) -> Int {
         switch (purchaseAmount % 10) {
