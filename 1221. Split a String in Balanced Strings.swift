f1b596ab0be30a777f7e1656526d186add9e57ea
MunavarPM <121251398+MunavarPM@users.noreply.github.com>
2024-02-23 09:56:08 +0530

    Create 1221. Split a String in Balanced Strings.swift

---
+++ 1221. Split a String in Balanced Strings.swift
@@ -0,0 +1,48 @@
+/*
+Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s, split it into some number of substrings such that:
+
+Each substring is balanced.
+Return the maximum number of balanced strings you can obtain.
+
+ 
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", each substring contains same number of 'L' and 'R'.
+Note that s cannot be split into "RL", "RR", "RL", "LR", "LL", because the 2nd and 5th substrings are not balanced.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+*/
+
+
+class Solution {
+    func balancedStringSplit(_ s: String) -> Int {
+        var result = 0
+        var Rcount = 0
+        var Lcount = 0
+        
+        for char in s {
+            if char == "R" {
+                Rcount += 1
+            } else {
+                Lcount += 1
+            }
+            if Rcount == Lcount {
+                result += 1
+            }
+        }
+        return result
+    }
+}
